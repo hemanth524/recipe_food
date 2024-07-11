96ab4a2115a6a9f49c80a8c95890ab8bf5b8dcbe
hemanth524 <130144834+hemanth524@users.noreply.github.com>
2024-07-11 15:47:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
 
     <App/> 
    
 
   <ToastContainer />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 
